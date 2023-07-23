9327f6215ab6118e873b269623314e63f5265cc4
Tim Shaw <timothyshaw0225@gmail.com>
2023-07-23 14:22:16 -0400

    screenshot only game section

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from utility import *
+import copy
 
 dim = get_dimensions()
 print(dim)
-get_screen_coords()
+
+s_dim = copy.deepcopy(dim)
+s_dim[0] += round(dim[2] / 17.66)
+s_dim[1] = round(dim[3] / 20.05)
+s_dim[2] = round(dim[2] / 1.13)
+s_dim[3] = round(dim[3] / 1.05)
+
+sc = get_screenshot(s_dim)
+sc.show()
