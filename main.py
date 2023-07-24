f81466fa7bc1c5eb8707aa132d400a43ed3fa369
TimShaw1 <timothyshaw0225@gmail.com>
2023-07-24 14:40:18 -0400

    done

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def getpixel(x,y):
 
 dim = get_dimensions()
 
+LEVEL = "3"
+
 bomb_col = (45,58,126)
 egg_col = (255,255,255)
 bg = (99, 111, 176)
@@ -17,8 +19,17 @@ bg2 = (51, 99, 146)
 
 x = dim[0] + round(dim[2] / 2.34)
 x2 = dim[0] + round(dim[2] / 2.74)
-y = dim[1] + round(dim[3] / 1.87)
-pyautogui.moveTo(x,y)
+
+y_1 = dim[1] + round(dim[3] / 1.65)
+y_2 = dim[1] + round(dim[3] / 1.7)
+y_3 = dim[1] + round(dim[3] / 1.8)
+pyautogui.moveTo(x,y_3)
+
+level_ys = {"1": y_1, "2": y_2, "3": y_3}
+level_times = {"1": 8/60, "2": 6/60, "3": 5/60}
+
+y = level_ys[LEVEL]
+wait_time = level_times[LEVEL]
 
 while not keyboard.is_pressed('`'):
     t = time.time()
@@ -33,5 +44,5 @@ while not keyboard.is_pressed('`'):
     if pixel2[0] > 200 and pixel2[1] > 180 and pixel2[2] > 180:
         keyboard.press('c')
         print("egg")
-        time.sleep(5/60)
+        time.sleep(wait_time)
     #print(time.time() - t)
