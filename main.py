3848465dc98a1a3a729ebfc7a7442e8488226a80
TimShaw1 <timothyshaw0225@gmail.com>
2023-07-24 14:31:22 -0400

    works lmao

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from utility import *
 import copy
 import keyboard
 import time
-from PIL import ImageGrab
+from ctypes import windll
+dc= windll.user32.GetDC(0)
+
+def getpixel(x,y):
+    return tuple(int.to_bytes(windll.gdi32.GetPixel(dc,x,y), 3, "little"))
 
 dim = get_dimensions()
 
@@ -11,48 +15,23 @@ egg_col = (255,255,255)
 bg = (99, 111, 176)
 bg2 = (51, 99, 146)
 
-x = dim[0] + round(dim[2] / 2.74)
-y = dim[1] + round(dim[3] / 1.67)
+x = dim[0] + round(dim[2] / 2.34)
+x2 = dim[0] + round(dim[2] / 2.74)
+y = dim[1] + round(dim[3] / 1.87)
 pyautogui.moveTo(x,y)
 
-# short = 12-13 frames -- 8 from the apex
-# long = 18-23 frames -- 13 from the apex
-
-top_x = dim[0] + round(dim[2] / 1.79)
-top_y = dim[1] + round(dim[3] / 5.83)
-
-mid_x = dim[0] + round(dim[2] / 1.76)
-mid_y = dim[1] + round(dim[3] / 3.57)
-
-low_x = dim[0] + round(dim[2] / 1.87)
-low_y = dim[1] + round(dim[3] / 2.84)
-
-egg = False
-egg_times = []
-
-def get_pixels():
-    pixel_top = ImageGrab.grab().getpixel((top_x, top_y))
-    pixel_mid = ImageGrab.grab().getpixel((mid_x, mid_y))
-    pixel_low = ImageGrab.grab().getpixel((low_x, low_y))
-
-    pix_list = [pixel_low, pixel_mid, pixel_top]
-    for i in range(len(pix_list)):
-        if pix_list[i][0] > 200 and pix_list[i][1] > 180 and pix_list[i][2] > 180:
-            egg_times.append(time.time() + 6/60 + (3/60)*i)
-            egg_times.sort()
-            print("egg")
-            continue
-
 while not keyboard.is_pressed('`'):
-    get_pixels()
-
     t = time.time()
-    offset = 0
-    if len(egg_times) > 0:
-        if egg_times[0] <= t:
-            pyautogui.keyDown('c')
-            egg_times.pop(0)
-            t2 = time.time()
-            while time.time() - t2 < 1/15:
-                get_pixels()
-            pyautogui.keyUp('c')
+    pixel = getpixel(x, y)
+    pixel2 = getpixel(x2, y)
+    #if keyboard.is_pressed('c'):
+    keyboard.release('c')
+    if pixel[0] > 200 and pixel[1] > 180 and pixel[2] > 180:
+        keyboard.press('c')
+        print("egg")
+        time.sleep(5/60)
+    if pixel2[0] > 200 and pixel2[1] > 180 and pixel2[2] > 180:
+        keyboard.press('c')
+        print("egg")
+        time.sleep(5/60)
+    #print(time.time() - t)
